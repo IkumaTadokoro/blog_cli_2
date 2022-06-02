516c4e60d1f4bda9001a5c4b93c487b4007401ff
ikuma-t <tadokorodev@gmail.com>
2022-06-02 23:59:41 +0900

    test: add help command test

--- lib/blog_cli_2/cli.rb
+++ lib/blog_cli_2/cli.rb
@@ -8,7 +8,9 @@ module BlogCli2
   class CLI < Thor
     BLOG_URL = "https://ikuma-t.work"
 
-    desc "open", "open blog in browser"
+    class_option :help, type: :boolean, aliases: "-h", desc: "help message."
+
+    desc "open", "Open blog in browser"
     def open
       options = { dry_run: ENV["LAUNCHY_DRY_RUN"] == "1" }
       Launchy.open(BLOG_URL, options)
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -6,6 +6,22 @@ require "launchy"
 RSpec.describe "Cli", type: :aruba do
   before { set_environment_variable "LAUNCHY_DRY_RUN", "1" }
 
+  describe "#help" do
+    expected = <<~EXPECTED
+      Commands:
+        blog help [COMMAND]  # Describe available commands or one specific command
+        blog open            # Open blog in browser
+
+      Options:
+        -h, [--help], [--no-help]  # help message.
+    EXPECTED
+
+    before { run_command("blog help") }
+
+    it { expect(last_command_started).to be_successfully_executed }
+    it { expect(last_command_started).to have_output(expected) }
+  end
+
   describe "#open" do
     before { run_command("blog open") }
 
